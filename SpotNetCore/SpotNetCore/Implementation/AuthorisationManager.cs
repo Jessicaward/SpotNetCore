a66b5ff8244e00860db08be4a59ab5d13105a1f9
Jessica Ward <Jessicawardiom@gmail.com>
2020-11-22 22:48:25 +0000

    Added URI builder

--- SpotNetCore/SpotNetCore/Implementation/AuthorisationManager.cs
+++ SpotNetCore/SpotNetCore/Implementation/AuthorisationManager.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Extensions.Logging;
 
 namespace SpotNetCore.Implementation
@@ -10,7 +11,23 @@ namespace SpotNetCore.Implementation
         {
             _logger = logger;
         }
-        
-        
+
+        private string BuildAuthorisationUri(string clientId, string redirectUri, string codeChallenge, string state, string scopes)
+        {
+            return new UriBuilder()
+            {
+                Scheme = "https",
+                Host = "accounts.spotify.com",
+                Path = "authorize",
+                Query = BuildAuthorisationQuery(clientId, redirectUri, codeChallenge, state, scopes)
+            }.Uri.ToString();
+        }
+
+        private static string BuildAuthorisationQuery(string clientId, string redirectUri, string codeChallenge, string state, string scopes)
+        {
+            return Uri.EscapeUriString("?client_id=" + clientId + "&response_type=code" 
+                   + "&redirect_uri=" + redirectUri + "&code_challenge_method=S256"
+                   + "&code_challenge= " + codeChallenge + "&state=" + state + "&scope=" + scopes);
+        }
     }
 }
