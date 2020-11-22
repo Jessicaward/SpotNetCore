017ab0d8257c73db55a2b4e159bf4ea9e0e41e31
Jessica Ward <Jessicawardiom@gmail.com>
2020-11-22 22:09:32 +0000

    Initial commit

---
+++ SpotNetCore/SpotNetCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SpotNetCore
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                //.AddSingleton<SomethingService>()
+                .BuildServiceProvider();
+        }
+    }
+}
