e78f5a8b0cd9135d84c54970b1b84f2ae025e436
Jessica Ward <Jessicawardiom@gmail.com>
2020-11-30 15:31:18 +0000

    #2 add console colour functionality

---
+++ SpotNetCore/SpotNetCore/Implementation/Terminal.cs
@@ -0,0 +1,64 @@
+using System;
+
+namespace SpotNetCore.Implementation
+{
+    /// <summary>
+    /// Terminal is suppoesed to act as a wrapper around Console. To provide colour, state etc.
+    /// </summary>
+    public class Terminal
+    {
+        public static void WriteRed(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+        }
+        
+        public static void WriteBlue(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(text);
+        }
+        
+        public static void WriteDarkBlue(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(text);
+        }
+        
+        public static void WriteGreen(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+        }
+        
+        public static void WriteMagenta(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(text);
+        }
+        
+        public static void WriteDarkGreen(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(text);
+        }
+        
+        public static void WriteDarkRed(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(text);
+        }
+        
+        public static void WriteYellow(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(text);
+        }
+
+        public static void WriteWhite(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(text);
+        }
+    }
+}
